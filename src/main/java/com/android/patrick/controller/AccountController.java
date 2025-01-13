6ca4c1000bd7f196bfa08d0639cf4f795ddef921
Patrick <bautistapatrick03@gmail.com>
2025-01-13 11:58:17 +0800

    initial commit

---
+++ src/main/java/com/android/patrick/controller/AccountController.java
@@ -0,0 +1,15 @@
+package com.android.patrick.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/account")
+public class AccountController {
+
+    @GetMapping("/hello")
+    public  String sayHello(){
+        return "Hello world";
+    }
+}
