e2b3e21f82fcb757394e96e35e1b039ff159d0d4
john.bautista <bautistapatrick03@gmail.com>
2025-01-14 11:15:27 +0800

    test

--- src/main/java/com/restapi/cicd/controller/UserController.java
+++ src/main/java/com/restapi/cicd/controller/UserController.java
@@ -45,7 +45,6 @@ public class UserController extends BasedController{
 
     @PostMapping("/login")
     public ResponseEntity<?>signIn(){
-        logger.debug("testing git");
         return ResponseEntity.ok("ok");
     }
 }
